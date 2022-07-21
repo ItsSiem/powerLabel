9c633d28866c0afbfd20769725b33fc7a2bf41c1
Siem Gerritsen <siem2004gerritsen@gmail.com>
2022-07-21 15:19:00 +0200

    Added more specific error messages

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -37,122 +37,101 @@ namespace powerLabel
 
         private void scanSystem(object sender, RoutedEventArgs e)
         {
-            ComputerSystem.GetSystem();
-            ComputerSystem system = ComputerSystem.system;
+            try
+            {
+                ComputerSystem.GetSystem();
+                ComputerSystem system = ComputerSystem.system;
 
-            // Model Label string processing
-            string modelString = system.motherboard.model;
-            modelString = ComputerSystem.getShortString(modelString, new string[] {
+                // Model Label string processing
+                string modelString = system.motherboard.model;
+                modelString = ComputerSystem.getShortString(modelString, new string[] {
                 @"HP Z\w+( \d{2}\w? G\d)?",     // HP Systems (HP Z840, HP ZBook 15 G3, HP ZBook 14U G5)
                 @"Precision \w* \w*"            // DELL Systems (Precision WorkStation T3500, Precision Tower 3620)
-            });
+                });
 
-            // CPU Label string processing
-            string cpuString = system.processor.name;
-            cpuString = ComputerSystem.getShortString(cpuString, new string[] {
+                // CPU Label string processing
+                string cpuString = system.processor.name;
+                cpuString = ComputerSystem.getShortString(cpuString, new string[] {
                 @"(((Platinum)|(Gold)|(Silver)|(Bronze)) \w*-*\d+\w*)|(\w+-*\d{3,}\w*( v\d)*)" // regex voor vrijwel alle core i en xeon processoren vanaf 2006
-            });
-            if (system.processorAmount > 1)
-            {
-                cpuString = cpuString.Insert(0, "2x ");
-            }
-
-            // RAM Label string processing
-            string ramString = system.memoryModules.Sum(item => Convert.ToInt64(item.module.capacity)) / 1073741824 + "GB " + MemoryModule.memoryTypeLookup[system.memoryModules.First().module.memoryType];
-
-            // Disk Label string processing
-            string diskString = "";
-            List<string> disks = new List<string>();
-            List<string> doneDisks = new List<string>();
-            foreach (DiskConfig disk in system.diskConfigs)
-            {
-                disks.Add(disk.ToString());
-            }
-            foreach (string disk in disks)
-            {
-                if (!doneDisks.Any(a => a == disk))
+                });
+                if (system.processorAmount > 1)
                 {
-                    if (disks.Where(a => a == disk).Count() > 1)
-                    {
-                        int multiplier = disks.Where(a => a == disk).Count();
-                        diskString += $"{multiplier}x {disk}\r\n";
-                        doneDisks.Add(disk);
-                    }
-                    else
-                    {
-                        diskString += disk + "\r\n";
-                        doneDisks.Add(disk);
-                    }
+                    cpuString = cpuString.Insert(0, "2x ");
                 }
 
-            }
+                // RAM Label string processing
+                string ramString = system.memoryModules.Sum(item => Convert.ToInt64(item.module.capacity)) / 1073741824 + "GB " + MemoryModule.memoryTypeLookup[system.memoryModules.First().module.memoryType];
+
+                // Disk Label string processing
+                string diskString = "";
+                List<string> disks = new List<string>();
+                List<string> doneDisks = new List<string>();
+                foreach (DiskConfig disk in system.diskConfigs)
+                {
+                    disks.Add(disk.ToString());
+                }
+                foreach (string disk in disks)
+                {
+                    if (!doneDisks.Any(a => a == disk))
+                    {
+                        if (disks.Where(a => a == disk).Count() > 1)
+                        {
+                            int multiplier = disks.Where(a => a == disk).Count();
+                            diskString += $"{multiplier}x {disk}\r\n";
+                            doneDisks.Add(disk);
+                        }
+                        else
+                        {
+                            diskString += disk + "\r\n";
+                            doneDisks.Add(disk);
+                        }
+                    }
+
+                }
 
 
-            // GPU Label string processing
-            string gpuString = "";
-            foreach (VideoControllerConfig gpu in system.videoControllerConfigs)
-            {
-                gpuString += ComputerSystem.getShortString(gpu.videoController.name, new string[] {
+                // GPU Label string processing
+                string gpuString = "";
+                foreach (VideoControllerConfig gpu in system.videoControllerConfigs)
+                {
+                    gpuString += ComputerSystem.getShortString(gpu.videoController.name, new string[] {
                     @"\w{2,3} Graphics \w+",        // Intel intergrated graphics (HD Graphics 405, Pro Graphics 600)
                     @"Quadro (RTX )*\w+",           // Quadro's (Quadro RTX 4000, Quadro K2200, Quadro M2000M)
                     @"GeForce \wTX \d{3,}( \w+)*"   // Nvidia GeForce GTX / RTX 3060 Ti
                 }) + "\r\n";
+                }
+
+
+
+                modelField.Content = modelString;
+                cpuField.Content = cpuString;
+                ramField.Content = ramString;
+                disksField.Content = diskString;
+                gpuField.Content = gpuString;
+
+                int fontsize = 18;
+                cpuField.FontSize = fontsize;
+                ramField.FontSize = fontsize;
+                disksField.FontSize = fontsize;
+                gpuField.FontSize = fontsize;
             }
-
-
-
-            modelField.Content = modelString;
-            cpuField.Content = cpuString;
-            ramField.Content = ramString;
-            disksField.Content = diskString;
-            gpuField.Content = gpuString;
-
-            int fontsize = 18;
-            cpuField.FontSize = fontsize;
-            ramField.FontSize = fontsize;
-            disksField.FontSize = fontsize;
-            gpuField.FontSize = fontsize;
-            Database.AddSystem(system);
-            ComputerSystem.GetSystem();
-
-            using (var db = new ComputerSystemContext())
+            catch (Exception ex)
             {
-                db.ComputerSystems.Attach(ComputerSystem.system);
-
-                eventsStackpanel.Children.Clear();
-                foreach (Event evnt in db.Events.Where(a => a.computerSystem.id == ComputerSystem.system.id))
-                {
-                    StackPanel stackPanel = new StackPanel();
-                    stackPanel.Children.Add(new Label { Content = evnt.name });
-                    stackPanel.Children.Add(new Label { Content = evnt.employee });
-                    stackPanel.Children.Add(new Label { Content = evnt.date.ToShortDateString() });
-                    Border border = new Border { Background = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#ffe6cc")),
-                                                 CornerRadius = new CornerRadius(8),
-                                                 BorderThickness = new Thickness(2),
-                                                 Margin = new Thickness(5, 0, 10, 5)};
-                    border.Child = stackPanel;
-                    eventsStackpanel.Children.Add(border);
-                }
-
-                configHistoryStackPanel.Children.Clear();
-                foreach (ComputerSystem config in db.ComputerSystems.Where(a => a.motherboard.id == ComputerSystem.system.motherboard.id))
-                {
-                    Ellipse ellipse = new Ellipse
-                    {
-                        Width = 10,
-                        Height = 10,
-                        StrokeThickness = 2,
-                        Stroke = new SolidColorBrush(Color.FromRgb(0, 0, 0)),
-                        Fill = new SolidColorBrush(Color.FromRgb(255, 255, 255)),
-                        HorizontalAlignment = HorizontalAlignment.Left,
-                        VerticalAlignment = VerticalAlignment.Top,
-                        Margin = new Thickness(5 ,30 ,5, 50)
-                    };
-
-                    configHistoryStackPanel.Children.Add(ellipse);
-
-                }
+                MessageBox.Show($"An error occured while parsing the system specifications. \r\n {ex}");
             }
+
+            Database.AddSystem(ComputerSystem.system);
+            ComputerSystem.GetSystem();
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+            updateEvents();
         }
 
         private void settingsBtn_Click(object sender, RoutedEventArgs e)
@@ -416,7 +395,7 @@ namespace powerLabel
                 MessageBox.Show($"An exeption was encountered while saving the order.\r\n{ex}");
             }
             updateEvents();
-            
+
         }
 
         private void updateEvents()
@@ -442,6 +421,25 @@ namespace powerLabel
                     border.Child = stackPanel;
                     eventsStackpanel.Children.Add(border);
                 }
+
+                configHistoryStackPanel.Children.Clear();
+                foreach (ComputerSystem config in db.ComputerSystems.Where(a => a.motherboard.id == ComputerSystem.system.motherboard.id))
+                {
+                    Ellipse ellipse = new Ellipse
+                    {
+                        Width = 10,
+                        Height = 10,
+                        StrokeThickness = 2,
+                        Stroke = new SolidColorBrush(Color.FromRgb(0, 0, 0)),
+                        Fill = new SolidColorBrush(Color.FromRgb(255, 255, 255)),
+                        HorizontalAlignment = HorizontalAlignment.Left,
+                        VerticalAlignment = VerticalAlignment.Top,
+                        Margin = new Thickness(5, 30, 5, 50)
+                    };
+
+                    configHistoryStackPanel.Children.Add(ellipse);
+
+                }
             }
         }
 
@@ -451,7 +449,7 @@ namespace powerLabel
             using (var db = new ComputerSystemContext())
             {
                 List<Event> events = db.Events.Where(a => Regex.IsMatch(a.name, ".*scan.*")).OrderByDescending(b => b.date).Include(c => c.computerSystem).ToList();
-                Event prevEvent = new Event { date = DateTime.Parse("1990-01-01")};
+                Event prevEvent = new Event { date = DateTime.Parse("1990-01-01") };
 
                 foreach (Event evnt in events)
                 {
@@ -469,7 +467,7 @@ namespace powerLabel
                     {
                         StackPanel dayMarker = new StackPanel { Orientation = Orientation.Horizontal };
                         dayMarker.Children.Add(new Separator { Width = 300 });
-                        dayMarker.Children.Add(new TextBlock { Text = evnt.date.ToShortDateString(), HorizontalAlignment = HorizontalAlignment.Right});
+                        dayMarker.Children.Add(new TextBlock { Text = evnt.date.ToShortDateString(), HorizontalAlignment = HorizontalAlignment.Right });
 
                         recentSystemsPanel.Children.Add(dayMarker);
                         /*
@@ -480,10 +478,12 @@ namespace powerLabel
                         */
                     }
 
-                    StackPanel stackPanel = new StackPanel { Margin = new Thickness(5)};
-                    stackPanel.Children.Add(new TextBlock { Text = $"{time} - {evnt.employee}",
-                                                            FontSize = 9,
-                                                            Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF4C4C4C"))
+                    StackPanel stackPanel = new StackPanel { Margin = new Thickness(5) };
+                    stackPanel.Children.Add(new TextBlock
+                    {
+                        Text = $"{time} - {evnt.employee}",
+                        FontSize = 9,
+                        Foreground = new SolidColorBrush((Color)ColorConverter.ConvertFromString("#FF4C4C4C"))
                     });
                     stackPanel.Children.Add(new TextBlock { Text = system.motherboard.model });
                     stackPanel.Children.Add(new TextBlock { Text = $"{system.processorAmount}x {system.processor.name}" });
@@ -524,7 +524,7 @@ namespace powerLabel
                                     <TextBlock Text="CZC7077LBD"/>
                                 </StackPanel>
             */
-            }
+        }
 
         private void boldCheckbox_Changed(object sender, RoutedEventArgs e)
         {
